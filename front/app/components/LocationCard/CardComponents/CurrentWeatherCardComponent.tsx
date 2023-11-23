7a268f2e2e86aaaa8e65f55409d9e39020bb113b
Simon Damberg <damberg.simon00@gmail.com>
2023-11-23 10:56:30 +0100

    Fixed locale for temp data

--- front/app/components/LocationCard/CardComponents/CurrentWeatherCardComponent.tsx
+++ front/app/components/LocationCard/CardComponents/CurrentWeatherCardComponent.tsx
@@ -1,22 +1,27 @@
 import React from "react";
 import Image from "next/image";
 import { providerToBorderColor } from "@/app/utils/colors";
+import { useTranslation } from "react-i18next";
 
 interface ICurrentWeatherCardProps {
-  airTemperature: string;
+  airTemperature: number;
   symbol: Number;
   provider: string;
 }
 
 const CurrentWeatherCard = (props: ICurrentWeatherCardProps) => {
   const { airTemperature, symbol, provider } = props;
+  const { t, i18n } = useTranslation();
+
   return (
     <div
-      className={`flex flex-col bg-sky-800 rounded-xl p-4 mx-4 border-2 ${
-        providerToBorderColor[provider.toLowerCase()]
-      }`}>
+      className={`flex flex-col bg-sky-800 rounded-xl p-4 mx-4 border-2 ${providerToBorderColor[provider.toLowerCase()]
+        }`}>
       <div className="flex flex-row justify-center">
-        <p className="text-sky-100 text-2xl mx-2">{airTemperature}°C</p>
+        <p className="text-sky-100 text-2xl mx-2">{airTemperature.toLocaleString(i18n.language, {
+          minimumFractionDigits: 1,
+          maximumFractionDigits: 1,
+        })}°C</p>
         <div className="flex flex-col justify-center">
           <Image
             src={`/images/weatherSymbols/${symbol}.png`}
