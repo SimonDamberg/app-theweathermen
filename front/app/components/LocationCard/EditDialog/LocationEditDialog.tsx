29c41ec67152df254280be780439b75c18188200
Simon Damberg <damberg.simon00@gmail.com>
2023-11-27 12:17:43 +0100

    fixed margin of buttons

--- front/app/components/LocationCard/EditDialog/LocationEditDialog.tsx
+++ front/app/components/LocationCard/EditDialog/LocationEditDialog.tsx
@@ -159,7 +159,7 @@ const LocationEditDialog = (props: ILocationEditDialogProps) => {
                   className={`bg-${colour}-950 px-2 py-3 sm:flex sm:flex-row-reverse`}>
                   <button
                     type="button"
-                    className={`mt-3 inline-flex w-full justify-center rounded-md bg-${colour}-800 hover:opacity-80 px-3 py-2 text-sm font-semibold text-${colour}-100 shadow-sm sm:ml-3 sm:w-auto mt-0`}
+                    className={`inline-flex w-full justify-center rounded-md bg-${colour}-800 hover:opacity-80 px-3 py-2 text-sm font-semibold text-${colour}-100 shadow-sm sm:ml-3 sm:w-auto`}
                     onClick={() => setOpen(false)}
                     ref={cancelButtonRef}>
                     {t("back")}
