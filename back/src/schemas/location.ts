5c784ad72df12569538514fbbf462d5b144a4f2b
Simon Damberg <damberg.simon00@gmail.com>
2023-11-30 18:01:04 +0100

    fixed build errors

--- back/src/schemas/location.ts
+++ back/src/schemas/location.ts
@@ -1,15 +1,14 @@
-import { Schema, model, InferSchemaType } from 'mongoose';
+import { Schema, model, InferSchemaType } from "mongoose";
 
 // Create a Schema corresponding to the document interface.
-export const LocationSchema = new Schema(
-  {
-    name: { type: String, required: true },
-    lat: { type: Number, required: true },
-    lon: { type: Number, required: true },
-  }
-);
+export const LocationSchema = new Schema({
+  _id: { type: String, required: true },
+  name: { type: String, required: true },
+  lat: { type: Number, required: true },
+  lon: { type: Number, required: true },
+});
 
 export type LocationType = InferSchemaType<typeof LocationSchema>;
 
 // Create a Model.
-export const Location = model<LocationType>('Location', LocationSchema);
+export const Location = model<LocationType>("Location", LocationSchema);
